ca4b9a9d47f7cfac6bc2a4ad5b9402fa368df834
akram <akramkhalid12@gmail.com>
2019-08-03 22:12:39 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import logo from './logo.svg';
+import {TweenMax, Power3} from 'gsap';
 import './App.css';
 
 function App() {
+  let logoItem = useRef(null);
+  let textItem = useRef(null);
+
+  useEffect(() => {
+    TweenMax.to(logoItem, .8, {opacity: 1, y:-20, ease: Power3.easeOut, });
+    TweenMax.to(textItem, .8, {opacity: 1, y:-20, delay:.2, ease: Power3.easeOut, });
+  },[]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img src={logo} className="App-logo" alt="logo" 
+        ref={item => {logoItem = item}}/>
+        <p ref={item => {textItem = item}}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
