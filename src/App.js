9516e885b7c8a5a67b3ded5dede61dc8d72554fe
akram <akramkhalid12@gmail.com>
2019-08-03 22:18:52 -0500

    Part 2; Consist of animating circles and eventHandlers

--- src/App.js
+++ src/App.js
@@ -1,33 +1,52 @@
-import React, {useEffect, useRef} from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useRef, useState} from 'react';
 import {TweenMax, Power3} from 'gsap';
 import './App.css';
 
 function App() {
-  let logoItem = useRef(null);
-  let textItem = useRef(null);
+  let app = useRef(null);
+  let circle = useRef(null);
+  let circleRed = useRef(null);
+  let circleBlue = useRef(null);
 
+  const [circleState, setCircleState] = useState(false)
+
+  const handleClick = () => { 
+    TweenMax.to(circleRed, .8, {width: 200, height: 200,  ease: Power3.easeInOut})
+    setCircleState(true);
+  }
+
+  const handleClickRevert = () => { 
+    TweenMax.to(circleRed, .8, {width: 75, height: 75,  ease: Power3.easeInOut})
+    setCircleState(false);
+  }
+ 
   useEffect(() => {
-    TweenMax.to(logoItem, .8, {opacity: 1, y:-20, ease: Power3.easeOut, });
-    TweenMax.to(textItem, .8, {opacity: 1, y:-20, delay:.2, ease: Power3.easeOut, });
+    TweenMax.to(app, 0, {css:{visibility: 'visible'}})
+    // TweenMax.from(circle, .8, {opacity: 0, ease: Power3.easeInOut, x: 40})
+    // TweenMax.from(circleRed, .8, {opacity: 0, ease: Power3.easeInOut, x: 40, delay: .2})
+    // TweenMax.from(circleBlue, .8, {opacity: 0, ease: Power3.easeInOut, x: 40, delay: .4})
+
+
+    TweenMax.staggerFrom([circle, circleRed, circleBlue ], .8, {opacity: 0, ease: Power3.easeOut, x: 40}, .2 )
   },[]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" 
-        ref={item => {logoItem = item}}/>
-        <p ref={item => {textItem = item}}>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div 
+    ref={el => {app = el}}
+    className="App">
+      <header className="App-header"> 
+        <div className="circle-container">
+          <div
+          ref={el => {circle = el}}
+          className="circle"></div>
+          <div 
+          ref={el => {circleRed = el}}
+          onClick={circleState !== true ? handleClick : handleClickRevert}
+          className="circle red"></div>
+          <div 
+          ref={el => {circleBlue = el}}
+          className="circle blue"></div>
+        </div>
       </header>
     </div>
   );
